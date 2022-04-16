b0719a044c403540174c4b27dce08b541b60af1e
Developer-Barsha <lamiatabassumbarsha@gmail.com>
2022-04-16 12:09:33 +0600

    context used

--- src/pages/Home/Dinners/Dinners.js
+++ src/pages/Home/Dinners/Dinners.js
@@ -1,17 +1,25 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import { CartContext } from '../../../App';
 import Meal from '../Meal/Meal';
 
 const Dinners = () => {
+    const [cart, setCart] = useContext(CartContext);
     const [dinners, setDinners] = useState([]);
-    useEffect(()=>{
+
+    const addToCart = (meal) => {
+        let addedItems = [...cart, meal];
+        setCart(addedItems);
+    };
+
+    useEffect(() => {
         fetch('dinner.json')
-        .then(res=>res.json())
-        .then(data=>setDinners(data))
-    } ,[]);
+            .then(res => res.json())
+            .then(data => setDinners(data))
+    }, []);
     return (
         <div className='meals'>
             {
-                dinners.map(meal=><Meal key={meal.id} meal={meal}></Meal>)
+                dinners.map(meal => <Meal key={meal.id} addToCart={addToCart} meal={meal}></Meal>)
             }
         </div>
     );
--- src/pages/Home/Lunches/Lunches.js
+++ src/pages/Home/Lunches/Lunches.js
@@ -1,18 +1,27 @@
-import React,{useState, useEffect} from 'react';
+import React, { useEffect, useState, useContext } from 'react';
+import { CartContext } from '../../../App';
 import './Lunches.css'
 import Meal from '../Meal/Meal';
 
 const Lunches = () => {
+    const [cart, setCart] = useContext(CartContext);
     const [lunches, setLunches] = useState([]);
+
     useEffect(()=>{
         fetch('lunch.json')
         .then(res=>res.json())
         .then(data=>setLunches(data))
     } ,[]);
+
+    const addToCart = (meal) => {
+        let addedItems=[...cart, meal];
+        setCart(addedItems);
+    };
+
     return (
         <div className='meals'>
             {
-                lunches.map(meal=><Meal key={meal.id} meal={meal}></Meal>)
+                lunches.map(meal=><Meal key={meal.id} addToCart={addToCart} meal={meal}></Meal>)
             }
         </div>
     );
